1f89796f49c4b0d00bb19e5cc32e579e7b41986e
zaryabkhan364 <khanzaryab007@gmail.com>
2025-03-07 10:43:39 +0500

    bug fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,28 @@
-import type { Configuration } from 'webpack';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type { Configuration, WebpackOptionsNormalized, ExternalItemFunctionData } from "webpack";
 
 const nextConfig = {
   webpack: (config: Configuration) => {
-    config.externals = [...(config.externals || []), /\.node$/]; // Ignore .node files
+    if (Array.isArray(config.externals)) {
+      config.externals.push(/\.node$/);
+    } else if (typeof config.externals === "function") {
+      const originalExternals = config.externals;
+      config.externals = (
+        data: ExternalItemFunctionData,
+        callback: (err?: Error | null, result?: string | boolean | string[] | Record<string, unknown>) => void
+      ) => {
+        if (data.request && /\.node$/.test(data.request)) {
+          return callback(null, `commonjs ${data.request}`);
+        }
+        if (typeof originalExternals === "function") {
+          return originalExternals(data, callback);
+        }
+        return callback();
+      };
+    } else {
+      config.externals = config.externals ? [config.externals, /\.node$/] : [/\.node$/];
+    }
+
     return config;
   },
 };
